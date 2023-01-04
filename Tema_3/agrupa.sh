618947d760c666d5c6e5a7fb87037e29ead5b9c5
Blanca <blancacano2.71828@gmail.com>
2023-01-05 00:05:38 +0100

    Elimino espacio en nombre de pdf

--- Tema_3/agrupa.sh
+++ Tema_3/agrupa.sh
@@ -4,7 +4,10 @@ salto="\newpage"
 cat cabecera.qmd  > $file 
 
 for f in ejercicio*.qmd; do
+    echo "" >> $file 
     echo $salto >> $file
     echo "Adding $f"
     cat $f >> $file
 done
+
+quarto render $file --to pdf
